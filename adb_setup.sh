18acd27d28685d64a7844b8eae29e716ee6969b5
Satoshi LNK <160682436+ikm-san@users.noreply.github.com>
2025-12-08 17:25:38 +0900

    Update NHK domains in adblock blacklist
    Removed specific NHK domains from the blacklist and added new entries.

--- adb_setup.sh
+++ adb_setup.sh
@@ -96,20 +96,10 @@ echo "t.co" >> /etc/adblock/adblock.whitelist
 # Add *.nhk related sites to blacklist for kids
 cat <<EOF >> /etc/adblock/adblock.blacklist
 nhk.or.jp
-www.nhk.or.jp
 nhk.jp
-*.nhk.or.jp
-*.nhk.jp
-*.nhk-cs.jp
-*.web.nhk
-www.web.nhk
-api.nhk.or.jp
-tvapi.nhk.or.jp
-service.nhk.or.jp
-track.nhk.or.jp
-www3.nhk.or.jp
-www2.nhk.or.jp
-www1.nhk.or.jp
+nhk-cs.jp
+web.nhk
+nhkworldpremium.com
 EOF
 
 # Define the cron job command
